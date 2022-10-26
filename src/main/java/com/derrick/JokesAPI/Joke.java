7d1001ae47668e3258849e88966be8bf5eee11ee
osoroderrick <osoroderrick@yahoo.com>
2022-10-26 10:12:29 -0400

    Jokes API

---
+++ src/main/java/com/derrick/JokesAPI/Joke.java
@@ -0,0 +1,48 @@
+package com.derrick.JokesAPI;
+//POJO - Plain old java object
+public class Joke {
+    private String icon_url;
+    private String id;
+    private String url;
+    private String value;
+
+    public Joke(){}
+
+    public String getIcon_url() {
+        return icon_url;
+    }
+
+    public void setIcon_url(String icon_url) {
+        this.icon_url = icon_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return " value='" + value;
+
+    }
+}
