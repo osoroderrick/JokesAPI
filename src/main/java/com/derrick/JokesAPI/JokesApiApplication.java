8cf93566cd262037c9d49bb3983083770df2c37b
osoroderrick <osoroderrick@yahoo.com>
2022-10-26 09:34:13 -0400

    Jokes API

---
+++ src/main/java/com/derrick/JokesAPI/JokesApiApplication.java
@@ -0,0 +1,13 @@
+package com.derrick.JokesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/derrick/JokesAPI/JokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derrick.JokesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
